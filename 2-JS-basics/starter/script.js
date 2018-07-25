01fac408936ea1512ac46cf06c292e636ebe4139
Mihai Prica <mihai.prica@live.com>
2018-07-24 18:46:19 -0700

    4th coding challenge

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -6,15 +6,15 @@ massJohn = 100;
 heightMark = 1.8;
 heightJohn = 1.85;
 
-var bmiMark = massMark / heightMark ^ 2;
-var bmiJohn = massJohn / heightJohn ^ 2;
+var bmiMark = massMark / (heightMark ^ 2);
+var bmiJohn = massJohn / (heightJohn ^ 2);
 
 var bmiMarkHigher = bmiMark > bmiJohn;
 console.log("Is Mark's BMI higher than John's?" + ' ' + bmiMarkHigher );
 */
 
 // Third coding challenge
-
+/*
 function calculateTip(bill) {
     var percentage;
     
@@ -39,4 +39,35 @@ var totals = [bills[0] + tips[0],
             bills[2] + tips[2]];
 
 console.log(totals);
+*/
+
+// 4th coding challenge
+
+var john = {
+    fullName: 'John Smith',
+    mass: 100,
+    height: 1.85,
+    calculateBMI: function () {
+        this.BMI = this.mass / (this.height ^ 2);
+        return this.BMI;
+    }
+};
+
+var mark = {
+    fullName: 'Mark Spencer',
+    mass: 100,
+    height: 1.85, 
+    calculateBMI: function () {
+        this.BMI = this.mass / (this.height ^ 2);
+        return this.BMI;
+    }
+};
+
+if (mark.calculateBMI() > john.calculateBMI()) {
+    console.log(mark.fullName + ' has a higher BMI of ' + mark.BMI);
+} else if ( john.BMI > mark.BMI) {
+    console.log(john.fullName + ' has a higher BMI of ' + john.BMI);
+} else {
+    console.log('Both have the same BMI');
+}
 
