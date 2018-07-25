57e11248c8e7923c2eba98c9fe18022f62d19c1c
Mihai Prica <mihai.prica@live.com>
2018-07-24 19:44:28 -0700

    5th coding challenge

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -42,7 +42,7 @@ console.log(totals);
 */
 
 // 4th coding challenge
-
+/*
 var john = {
     fullName: 'John Smith',
     mass: 100,
@@ -71,3 +71,78 @@ if (mark.calculateBMI() > john.calculateBMI()) {
     console.log('Both have the same BMI');
 }
 
+*/
+// 5th coding challenge
+
+var expensesJohn = {
+    fullName: 'John Smith',
+    bills: [ 124, 48, 268, 180, 42 ],
+    tips: [],
+    totals: [],
+    calculateTip: function(bill) {
+        var percentage;
+
+        if (bill < 50){ 
+            percentage = 0.2;
+        } else if (bill >= 50 && bill < 200) {
+            percentage = 0.15;
+        } else {
+            percentage = 0.1;
+        }
+    
+        return bill * percentage;
+    },
+    calculateTips: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            this.tips[i] = this.calculateTip(this.bills[i]);
+            this.totals[i] = this.bills[i] + this.tips[i];
+         }
+    }
+};
+
+var expensesMark = {
+    fullName: 'Mark Spencer',
+    bills: [ 77, 375, 110, 45 ],
+    tips: [],
+    totals: [],
+    calculateTip: function(bill) {
+        var percentage;
+
+        if (bill < 100){ 
+            percentage = 0.2;
+        } else if (bill >= 100 && bill < 300) {
+            percentage = 0.1;
+        } else {
+            percentage = 0.25;
+        }
+    
+        return bill * percentage;
+    },
+    calculateTips: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            this.tips[i] = this.calculateTip(this.bills[i]);
+            this.totals[i] = this.bills[i] + this.tips[i];
+         }
+    }
+}
+
+expensesJohn.calculateTips();
+expensesMark.calculateTips();
+
+console.log(expensesJohn.tips, expensesJohn.totals);
+console.log(expensesMark.tips, expensesMark.totals);
+
+function averageTip (tips) {
+    var total = 0;
+    for (var i = 0; i < tips.length; i++) {
+        total += tips[i];
+    }
+    return total/tips.length;
+}
+
+expensesJohn.avgTip = averageTip(expensesJohn.tips);
+expensesMark.avgTip = averageTip(expensesMark.tips);
+
+var name = expensesJohn.avgTip >expensesMark.avgTip  ? expensesJohn.fullName : expensesMark.fullName;
+
+console.log ( name + ' had the highest avg. tip');
