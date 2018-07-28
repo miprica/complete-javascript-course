52557638a7fd91aeb0f0b9275c014b0d9c5a6a7a
Mihai Prica <mihai.prica@live.com>
2018-07-27 19:05:26 -0700

    Fixes to coding solution

--- 5-advanced-JS/starter/challenge.js
+++ 5-advanced-JS/starter/challenge.js
@@ -10,7 +10,7 @@ b) the answers from which the player can choose the correct one (choose an adequ
 c) correct answer (I would use a number for this)
 */
 ( function() {
-var Question = function (text, answers, correctAnswer) {
+function Question(text, answers, correctAnswer) {
     this.text = text;
     this.answers = answers;
     this.correctAnswer = correctAnswer;
@@ -21,6 +21,14 @@ Question.prototype.displayQuestion = function() {
 
     for (var i = 0; i < this.answers.length; i++) {
         console.log((i + 1) + '. ' + this.answers[i]);
+    }
+}
+
+Question.prototype.checkAnswer = function(answer) {
+    if (answer === this.correctAnswer) {
+        console.log('Correct answer!');
+    } else {
+        console.log('Wrong answer!');
     }
 }
 
@@ -38,27 +46,18 @@ var questions = [
 /*
 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
 */
-function askQuestion() {
-    var randQ = Math.floor(Math.random() * questions.length);
-    questions[randQ].displayQuestion();
-    return randQ;
-}
+var randQ = Math.floor(Math.random() * questions.length);
+
+questions[randQ].displayQuestion();
+
 /*
 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
 
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 */
-function checkAnswer(q) { 
-    var answer = prompt('Please select the correct answer.');
+var answer = parseInt(prompt('Please select the correct answer.'));
 
-    if (answer == q.correctAnswer) {
-        console.log('Correct answer!');
-    } else {
-        console.log('Wrong answer!');
-    }
-}
-var q = askQuestion();
-checkAnswer(questions[q]);
+questions[randQ].checkAnswer(answer);
 
 /*
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
